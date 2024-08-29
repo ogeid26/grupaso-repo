fe6433ed12e8a488de05fa5526c98384e29207cd
ogeid26 <dmalvaradog26@gmail.com>
2024-08-29 12:10:34 -0300

    working button and led

--- proyecto/proyecto.ino
+++ proyecto/proyecto.ino
@@ -3,19 +3,27 @@
 const int leftButtonPin = 21;
 const int rightButtonPin = 22;
 
-const int servoPin = 18;
-
 int leftLastState = HIGH;
 int leftCurrentState;
 
 int rightLastState = HIGH;
 int rightCurrentState;
 
-Servo myServo;
+int ledPin = 27;
+
+int servoPin = 17;
+
+Servo servo;
 
 void setup() {
   pinMode(leftButtonPin, INPUT_PULLUP);
   pinMode(rightButtonPin, INPUT_PULLUP);
+
+  pinMode(ledPin, OUTPUT);
+
+  servo.attach(servoPin, 500, 2400);
+  int pos = 0;
+
 
   Serial.begin(115200);
 }
@@ -28,9 +36,19 @@ void loop() {
 
   if (leftLastState == LOW && leftCurrentState == HIGH) {
     Serial.println("Botón 1 presionado");
-  } else {
-    Serial.println("Botón 1 no presionado");
+    // for (pos = 0; pos <= 180; pos += 1) {
+    // servo.write(pos);
+    // delay(15);
+    digitalWrite(ledPin, HIGH);
   }
+   else {
+    Serial.println("Botón 1 no presionado");
+    //  for (pos = 180; pos >= 0; pos -= 1) {
+    // servo.write(pos);
+    // delay(15);
+    digitalWrite(ledPin, LOW);
+  }
+  
   leftLastState = leftCurrentState;
   delay(200);
 }
